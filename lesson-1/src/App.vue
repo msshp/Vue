0a50cd1ca8e0ea7f644c4d307d33ebe3032fdce6
msshp <eu.nevcskaya@yandex.ru>
2022-10-20 21:40:07 +0300

    Задание 1

---
+++ lesson-1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="display">
+      <input type="number" v-model="operand1" /> <!-- v-model связывает тег с переменной из скрипта -->
+      <input type="number" v-model="operand2" />
+    </div>
+
+    <p>Результат: {{ result }}</p>
+
+    <div class="keyboard">
+      <button v-on:click="result = Number(operand1) + Number(operand2)">+</button>
+      <button v-on:click="result = Number(operand1) - Number(operand2)">-</button>
+      <button v-on:click="result = Number(operand1) * Number(operand2)">*</button>
+      <button v-on:click="result = Number(operand1) / Number(operand2)">/</button>
+      <button v-on:click="result = Math.pow(operand1, operand2)">^</button>
+      <button v-on:click="result = Math.trunc(Number(operand1) / Number(operand2))">integer division</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
